4ea2a4533499f638802cfbd0d791461a7b511c0d
Lavany Sharad Kashid <lavanykashid@gmail.com>
2026-07-08 23:32:51 +0530

    Prepare frontend for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register/>}/>
-        {/* <Route path="/coach" element={<Coach/>}/> */}
+        
         
         <Route path="/landing" element={
           <ProtectedRoute>
